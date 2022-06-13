312e2e78d239dc7d5de60a82459904d291ca9441
bozhedom <100995362+bozhedom@users.noreply.github.com>
2022-06-14 00:16:09 +1000

    observer

--- Program.cs
+++ Program.cs
@@ -4,133 +4,158 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Strategy
+namespace Observer
 {
-    interface FlyBehavior
+    interface Observer
     {
-        void fly();
+        void update(float temp, float humidity, float pressuare);
     }
-    interface QuackBehavior
+    interface DisplayElement
     {
-        void quack();
-        //
+        void display();
     }
-
-    class FlywithWings : FlyBehavior
+    interface Subject
     {
-        public void fly()
-        {
-            Console.WriteLine("I'm flying");
-        }
-    }
-
-    class FlyNoWay : FlyBehavior
-    {
-        public void fly()
-        {
-            Console.WriteLine("I can't flying");
-        }
-    }
-
-    class Quack : QuackBehavior
-    {
-        public void quack()
-        {
-            Console.WriteLine("quack!!!!!!!!!!!!!!!");
-        }
-    }
-
-    class MuteQuack : QuackBehavior
-    {
-        public void quack()
-        {
-            Console.WriteLine("tsssss");
-        }
-    }
-
-    class Squeak : QuackBehavior
-    {
-        public void quack()
-        {
-            Console.WriteLine("SQUEAKKKKKKKKKKKKK");
-        }
-    }
-
-
-    abstract class Duck
-    {
-        public FlyBehavior flyBehavior;
-        public QuackBehavior quackBehavior;
-
-        public void setFlyBehavior(FlyBehavior fb)
-        {
-            flyBehavior = fb;
-        }
-
-        public void setQuackBehavior(QuackBehavior qb)
-        {
-            quackBehavior = qb;
-        }
-
-        public void performFly()
-        {
-            flyBehavior.fly();
-        }
-        public void performQuack()
-        {
-            quackBehavior.quack();
-        }
-
-        public void swim()
-        {
-            Console.WriteLine("I'm swimming");
-        }
-
-        public abstract void display();
+        void addObserver(Observer o);
+        void removeObserver(Observer o);
+        void notifyObservers();
 
     }
 
-
-    class MalladDuck : Duck
+    class WeatherData : Subject
     {
-        public MalladDuck()
+        List<Observer> observers;
+        private float temperature;
+        private float humidity;
+        private float pressure;
+
+        public WeatherData()
         {
-            flyBehavior = new FlyNoWay();
-            quackBehavior = new MuteQuack();
+            observers = new List<Observer>();
         }
-        override public void display()
+
+        public void addObserver(Observer o)
         {
-            Console.WriteLine("I'm a MalladDuck");
+            observers.Add(o);
+        }
+        public void removeObserver(Observer o)
+        {
+            int i = observers.IndexOf(o);
+            if (i >= 0)
+            {
+                observers.Remove(o);
+            }
+        }//????
+        public void notifyObservers()
+        {
+            for (int i = 0; i < observers.Count(); ++i)
+            {
+                Observer observer = observers[i];
+                observer.update(temperature, humidity, pressure);
+            }
+        }
+
+        public void measurementsChanged()
+        {
+            notifyObservers();
+        }
+
+        public void setMeasurements(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            this.pressure = pressure;
+            measurementsChanged();
         }
     }
 
-    class ModelDuck : Duck
+    class CurrentConditionsDisplay : Observer, DisplayElement
     {
-        public ModelDuck()
+        private float temperature;
+        private float humidity;
+        private float pressure;
+        private Subject weatherData;
+
+        public CurrentConditionsDisplay(Subject weatherData)
         {
-            flyBehavior = new FlywithWings();
-            quackBehavior = new Squeak();
+            this.weatherData = weatherData;
+            weatherData.addObserver(this);
         }
-        override public void display()
+
+        public void update(float temperature, float humidity, float pressure)
         {
-            Console.WriteLine("I'm model duck!");
+            this.temperature = temperature;
+            this.humidity = humidity;
+            this.pressure = pressure;
+            display();
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Current conditions: " + temperature + "F degrees and " + humidity +
+                "% humidity and " + pressure + " pressure");
         }
     }
 
+    class StatisticsDisplay : Observer, DisplayElement
+    {
+        private float temperature;
+        Subject weatherData;
+
+        public StatisticsDisplay(Subject weatherData)
+        {
+            this.weatherData = weatherData;
+            weatherData.addObserver(this);
+        }
+
+        public void update(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            display();
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Temperature: " + temperature);
+        }
+
+    }
+
+    class ForecastDisplay: Observer, DisplayElement
+    {
+        private float humidity;
+        Subject weatherData;
+
+        public ForecastDisplay(Subject weatherData)
+        {
+            this.weatherData = weatherData;
+            weatherData.addObserver(this);
+        }
+
+        public void update(float temperature, float humidity, float pressure)
+        {
+            this.humidity = humidity;
+            display();
+        }
+
+        public void display()
+        {
+            Console.WriteLine("humidity: " + humidity);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Duck duck = new ModelDuck();
-            duck.performFly();
-            duck.performQuack();
-
-            Console.WriteLine("=========================");
-
-            duck.setFlyBehavior(new FlyNoWay());
-            duck.setQuackBehavior(new Quack());
-            duck.performFly();
-            duck.performQuack();
+            WeatherData weatherData = new WeatherData();
+            CurrentConditionsDisplay currentDisplay = 
+                new CurrentConditionsDisplay(weatherData);
+            StatisticsDisplay statisticsDisplay = 
+                new StatisticsDisplay(weatherData);
+            ForecastDisplay forecastDisplay = 
+                new ForecastDisplay(weatherData);
+            weatherData.setMeasurements(80, 62, 30); 
+            weatherData.setMeasurements(40, 13, 93);
             Console.Read();
         }
     }
